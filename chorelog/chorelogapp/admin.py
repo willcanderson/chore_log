7ff6ad24df949ca2538f7e8a516f3f6008c839d1
willcanderson <willcanderson@uchicago.edu>
2025-03-14 15:32:35 -0500

    add user model

--- chorelog/chorelogapp/admin.py
+++ chorelog/chorelogapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Parent, Child
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Parent)
+admin.site.register(Child)
--- chorelog/chorelogapp/models.py
+++ chorelog/chorelogapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('PARENT', 'Parent'),
+        ('CHILD', 'Child'),
+    )
+    user_type = models.CharField(max_length=6, choices=USER_TYPE_CHOICES)
+
+class Parent(models.Model):
+    """
+    Extends the base User model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='parent_profile')
+
+    def __str__(self):
+        return f"Parent: {self.user.get_full_name()}"
+    
+class Child(models.Model):
+    """
+    Extends base User model and references a parent
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='child_profile')
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
+
+    def __str__(self):
+        return f"Child: {self.user.get_full_name()} (Parent: {self.parent.user.get_full_name()})"
