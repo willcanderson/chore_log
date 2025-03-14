8d1d9c2bfe70eae96e829e220016ab29694faaf6
willcanderson <willcanderson@uchicago.edu>
2025-03-14 16:38:19 -0500

    add models for Chore_Definition, Work, Play

--- chorelog/chorelogapp/admin.py
+++ chorelog/chorelogapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Parent, Child
+from .models import User, Parent, Child, Chore_Definition, Work, Play
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Parent)
 admin.site.register(Child)
+admin.site.register(Chore_Definition)
+admin.site.register(Work)
+admin.site.register(Play)
--- chorelog/chorelogapp/models.py
+++ chorelog/chorelogapp/models.py
@@ -26,3 +26,28 @@ class Child(models.Model):
 
     def __str__(self):
         return f"Child: {self.user.get_full_name()} (Parent: {self.parent.user.get_full_name()})"
+    
+class Chore_Definition(models.Model):
+    name = models.CharField(max_length=50)
+    minute_value = models.IntegerField()
+    defined_by = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='defined_chores')
+
+    def __str__(self):
+        return f"Chore: {self.name}, worth {self.minute_value} minutes"
+
+class Work(models.Model):
+    chore = models.ForeignKey(Chore_Definition, on_delete=models.SET_NULL, null=True)
+    done_by = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='work_done')
+    date_logged = models.DateField.auto_now_add()
+
+    def __str__(self):
+        return f"Chore: {self.chore.name} logged on {self.date_logged}"
+
+class Play(models.Model):
+    game = models.CharField(max_length=50)
+    minutes_played = models.IntegerField()
+    child = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='play_done')
+    date_logged = models.DateField.auto_now_add()
+
+    def __str__(self):
+        return f"Chore: {self.game} logged on {self.date_logged}"
