57ee8e6d20e0970028dbd86be3ef9ba8011b7435
willcanderson <willcanderson@uchicago.edu>
2025-03-15 12:22:32 -0500

    simplify user model

--- chorelog/chorelogapp/admin.py
+++ chorelog/chorelogapp/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Parent, Child, Chore_Definition, Work, Play
+from .models import User, Chore_Definition, Work, Play
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Parent)
-admin.site.register(Child)
 admin.site.register(Chore_Definition)
 admin.site.register(Work)
 admin.site.register(Play)
--- chorelog/chorelogapp/models.py
+++ chorelog/chorelogapp/models.py
@@ -7,40 +7,19 @@ class User(AbstractUser):
         ('CHILD', 'Child'),
     )
     user_type = models.CharField(max_length=6, choices=USER_TYPE_CHOICES)
-
-class Parent(models.Model):
-    """
-    Extends the base User model
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='parent_profile')
-
-    def __str__(self):
-        return f"Parent: {self.user.get_full_name()}"
-    
-class Child(models.Model):
-    """
-    Extends base User model and references a parent
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='child_profile')
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
-
-    def __str__(self):
-        return f"Child: {self.user.get_full_name()} (Parent: {self.parent.user.get_full_name()})"
-    
-    class Meta:
-        verbose_name_plural = "children"
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True, blank=True)
     
 class Chore_Definition(models.Model):
     name = models.CharField(max_length=50)
     minute_value = models.IntegerField()
-    defined_by = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='defined_chores')
+    defined_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='defined_chores')
 
     def __str__(self):
         return f"Chore: {self.name}, worth {self.minute_value} minutes"
 
 class Work(models.Model):
     chore = models.ForeignKey(Chore_Definition, on_delete=models.SET_NULL, null=True)
-    done_by = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='work_done')
+    done_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='work_done')
     date_logged = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -49,7 +28,7 @@ class Work(models.Model):
 class Play(models.Model):
     game = models.CharField(max_length=50)
     minutes_played = models.IntegerField()
-    child = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='play_done')
+    child = models.ForeignKey(User, on_delete=models.CASCADE, related_name='play_done')
     date_logged = models.DateField(auto_now_add=True)
 
     def __str__(self):
