804cc74d7163c310dadfb7a6b868afd794748d6e
willcanderson <willcanderson@uchicago.edu>
2025-03-15 16:23:43 -0500

    make Django admin work with custom user

--- chorelog/chorelogapp/admin.py
+++ chorelog/chorelogapp/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Chore_Definition, Work, Play
 
-admin.site.register(User, UserAdmin)
+class MyUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('user_type','parent')}),
+    )
+    add_fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('user_type','parent')}),
+    )
+
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Chore_Definition)
 admin.site.register(Work)
 admin.site.register(Play)
